9c5e1d82ec389bd3d80f643638fc2562e944c835
Manelkl <Manel.kl.mk@gmail.com>
2023-10-30 09:23:15 -0700

    api

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -6,42 +6,47 @@ default RESTFul API actions
 
 from api.v1.views import app_views
 from models import storage
-from models.state import State
 from models.city import City
-from flask import abort, request, jsonify
+from models.state import State
+from flask import abort, jsonify, request
 
 
-@app_views.route("/states/<state_id>/cities", strict_slashes=False,
-                 methods=["GET"])
-def cities(state_id):
+@app_views.route("/states/<state_id>/cities", methods=["GET"],
+                 strict_slashes=False)
+def state_cities(state_id):
     """Method to get a state's cities"""
-    cities_list = []
     state = storage.get(State, state_id)
+
     if state is None:
         abort(404)
-    cities = state.cities
-    for city in cities:
-        cities_list.append(city.to_dict())
-    return jsonify(cities_list)
+
+    return jsonify([city.to_dict() for city in state.cities]), 200
 
 
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["GET"])
-def cities_id(city_id):
+@app_views.route("/cities/<city_id>", methods=["GET"],
+                 strict_slashes=False)
+def get_city(city_id):
     """Method to get city by id"""
     city = storage.get(City, city_id)
+
     if city is None:
         abort(404)
-    return jsonify(city.to_dict())
+
+    return jsonify(city.to_dict()), 200
 
 
-@app_views.route("/cities/<city_id>", strict_slashes=False, methods=["DELETE"])
-def city_delete(city_id):
+@app_views.route("/cities/<city_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_city(city_id):
     """Method to delete city by using id"""
-    obj = storage.get(City, city_id)
-    if obj is None:
+    city = storage.get(City, city_id)
+
+    if city is None:
         abort(404)
-    storage.delete(obj)
+
+    storage.delete(city)
     storage.save()
+
     return jsonify({}), 200
 
 
